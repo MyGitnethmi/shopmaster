ab92d64ae736d4148845829377f2217931bb19c4
seekerscloudlk <seekerscloudlkinfo@gmail.com>
2020-08-23 11:16:20 +0530

    17

--- client/src/proxy/ProxyHandler.java
+++ client/src/proxy/ProxyHandler.java
@@ -1,12 +1,49 @@
 package proxy;
 
 import service.ServiceFactory;
+import service.custom.CustomerService;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 
 public class ProxyHandler implements ServiceFactory {
+    private static ProxyHandler proxyHandler;
+    private ServiceFactory serviceFactory;
+    private CustomerService customerService;
+
+
+    private ProxyHandler() {
+        try {
+
+            serviceFactory = (ServiceFactory) Naming.lookup("rmi://localhost:5050/cmjd83");
+            customerService = serviceFactory.getService(ServiceType.CUSTOMER);
+
+
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 
     @Override
     public <T> T getService(ServiceType type) throws Exception {
-        return null;
+        switch (type) {
+            case CUSTOMER:
+                return (T) customerService;
+            case ORDER:
+                return null;
+            case ITEM:
+                return null;
+            default:
+                return null;
+        }
     }
 }
