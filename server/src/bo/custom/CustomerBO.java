6538befa88fbef88709330b3ff0d48df0f59b498
seekerscloudlk <seekerscloudlkinfo@gmail.com>
2020-08-23 09:47:10 +0530

    5

--- server/src/bo/custom/CustomerBO.java
+++ server/src/bo/custom/CustomerBO.java
@@ -1,7 +1,16 @@
 package bo.custom;
 
 import bo.SuperBo;
+import dto.CustomerDTO;
 
 public interface CustomerBO extends SuperBo {
+    public boolean saveCustomer(CustomerDTO dto) throws Exception;
 
+    public boolean updateCustomer(CustomerDTO dto) throws Exception;
+
+    public boolean deleteCustomer(String id) throws Exception;
+
+    public boolean getCustomer(String id) throws Exception;
+
+    public boolean getAllCustomers() throws Exception;
 }
