4d965815e5ec0752e34f793676886e3cff9ddab5
seekerscloudlk <seekerscloudlkinfo@gmail.com>
2020-08-23 11:09:17 +0530

    16

---
+++ client/src/proxy/ProxyHandler.java
@@ -0,0 +1,12 @@
+package proxy;
+
+import service.ServiceFactory;
+
+public class ProxyHandler implements ServiceFactory {
+
+
+    @Override
+    public <T> T getService(ServiceType type) throws Exception {
+        return null;
+    }
+}
